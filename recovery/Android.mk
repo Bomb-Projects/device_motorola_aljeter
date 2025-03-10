7a59c54d0acde9cf41ce153085baa23858bb6634
Christian Hoffmann <chrmhoffmann@gmail.com>
2025-03-10 16:33:16 -0300

    aljeter: Align with new recovery structure Change-Id: Ic3ecf63a2888aac08aeaf40677ae7bfa74912030

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_C_INCLUDES := \
-    bootable/recovery/edify/include \
+    bootable/deprecated-ota/edify/include \
     bootable/recovery/otautil/include
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_motorola
