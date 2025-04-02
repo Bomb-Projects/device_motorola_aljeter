9373d229fffc36dbf66aeaa019768ab8608c952b
Bruno Martins <bgcngm@gmail.com>
2025-04-02 09:38:59 -0300

    aljeter: Declare IMS libs as symlinks during extraction Change-Id: Ia1b8097d28dd4888232fe383b830515b1b9bafaa

--- Android.mk
+++ Android.mk
@@ -269,16 +269,6 @@ $(FIRMWARE_WIDEVINE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_WIDEVINE_SYMLINKS)
 
-IMS_LIBS := libimscamera_jni.so libimsmedia_jni.so
-IMS_SYMLINKS := $(addprefix $(TARGET_OUT_SYSTEM_EXT_APPS_PRIVILEGED)/ims/lib64/arm64/,$(notdir $(IMS_LIBS)))
-$(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "IMS lib link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /system/system_ext/lib64/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(IMS_SYMLINKS)
-
 WIFI_SYMLINKS := $(TARGET_OUT_VENDOR)/firmware/wlan/prima/
 $(WIFI_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Creating WCNSS Symlinks: $@"
